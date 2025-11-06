c64fc45d58688c0b0f4b0a4f2d34fbab0de48ed3
Kevin-nn <thekevincaldon@gmail.com>
2025-11-06 09:08:11 -0600

    actualizasion

--- src/app.modulo.ts
+++ src/app.modulo.ts
@@ -6,10 +6,13 @@ import { UsuariosModule } from './users/usuarios.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://tradefllow360_db_user:Cu5F82YvuNlYLfHh@store-mind.dqhqqgf.mongodb.net/store-mind?retryWrites=true&w=majority&appName=store-mind'), // Tu URL de MongoDB
+    MongooseModule.forRoot(
+      'mongodb+srv://tradefllow360_db_user:Cu5F82YvuNlYLfHh@store-mind.dqhqqgf.mongodb.net/store-mind?retryWrites=true&w=majority&appName=store-mind'), // Tu URL de MongoDB
     UsuariosModule,
   ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
+//env pasarr
